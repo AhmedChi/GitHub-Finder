b7f32ec6163363f786fc5207835833dd03cedbbe
Ahmed Abdel Rahman <62700312+AhmedChi@users.noreply.github.com>
2020-08-12 08:19:55 +0100

    Add Final
    This will be the last part with the completion of app, githubfinder and UI

--- app_githubfinder.js
+++ app_githubfinder.js
@@ -1,7 +1,6 @@
 const github = new Github;
 const ui = new UI;
 
-
 const searchUser = document.getElementById('searchUser');
 
 searchUser.addEventListener('keyup', (e) => {
@@ -14,6 +13,7 @@ searchUser.addEventListener('keyup', (e) => {
                 ui.showAlert('User not found', 'alert alert-danger');
             } else {
                 ui.showProfile(data.profile);
+                ui.showRepos(data.repos);
             }
         })
     } else {
